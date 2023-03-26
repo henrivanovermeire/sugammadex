83eb7ef344d3461f14b5625051957c149e84b411
henripointcare <henri@pointcare.be>
2023-03-26 23:08:20 +0200

    release 1

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import random
 
+import psycopg2
+
+from urllib.request import urlopen
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.units import cm
 import pandas as pd
 from PyPDF2 import PdfReader
 from math import ceil
+import json
 from pdf2image import convert_from_path
 from reportlab.lib.colors import purple
 attest_path = "source/bridion.pdf"
@@ -15,11 +19,19 @@ for i in range(len(images)):
     # Save pages as images in the pdf
     images[i].save('page' + str(i) + '.jpg', 'JPEG')
 
-patient_name = "HENRI VAN OVERMEIRE"
-patient_INSZ = "95100629538"
+conn = psycopg2.connect(
+    host="localhost",
+    database="ANASTHASIA",
+    user="postgres",
+    password="postgres")
+
+cursor = conn.cursor()
 
 
-def generate_sugammadex_attestation(attestnr, patient_name, prescriber_first_name, prescriber_last_name, datum, dosage, flacons):
+with urlopen("https://webappsa.riziv-inami.fgov.be/silverpages/Home/SearchHcw/?Form.Name=Van%20Overmeire&Form.FirstName=Henri&Form.Profession=&Form.Specialisation=&Form.ConventionState=&Form.Location=&Form.NihdiNumber=&Form.Qualification=&Form.NorthEastLat=&Form.NorthEastLng=&Form.SouthWestLat=&Form.SouthWestLng=&Form.LocationLng=&Form.LocationLat=") as response:
+    print(response.headers.items())
+    print(response.read())
+def generate_sugammadex_attestation(attestnr, patient_name, prescriber_first_name, prescriber_last_name, riziv, datum, dosage, flacons, reason):
 #creating a new canvas
     DOSAGE = dosage
 
@@ -32,14 +44,15 @@ def generate_sugammadex_attestation(attestnr, patient_name, prescriber_first_nam
     while not succesful_weight_hack:
         r = abs(random.gauss(mu=0, sigma=30))
         modified_patient_weight = patient_weight - r
-        print(modified_patient_weight)
         if ceil(modified_patient_weight*DOSAGE/200) == flacons:
             succesful_weight_hack = True
 
     patient_weight = int(modified_patient_weight)
 
-    print(r)
-    canvas = Canvas(f"{attestnr}.pdf", pagesize=A4)
+    riziv_case = riziv[0]
+    qualcode_case = riziv[1]
+
+    canvas = Canvas(f"{prescriber_last_name}{prescriber_first_name}_{attestnr}.pdf", pagesize=A4)
     page_width, page_height = canvas._pagesize
     canvas.setFont("Courier", 13)
     canvas.drawImage('page0.jpg', x=0, y=0, width=page_width, height=page_height)
@@ -52,7 +65,7 @@ def generate_sugammadex_attestation(attestnr, patient_name, prescriber_first_nam
     operation_date_year = datum[0:4]
     canvas.drawString(8.22 * cm, 10.33 * cm, f"{operation_date_day} {operation_date_month} {operation_date_year}", charSpace=1.7)
     canvas.setFont("Courier", 12)
-    canvas.drawString(1.5 * cm, 8.95 * cm, "GENERIEKE BRIDION ATTESTERINGSREDEN 1", charSpace=1.7)
+    canvas.drawString(1.5 * cm, 8.95 * cm, f"{reason}", charSpace=1.7)
     amount_substance = dosage * patient_weight
     amount_bottles = ceil(amount_substance / 200)
     canvas.drawString(5 * cm, 6.10 * cm, str(patient_weight), charSpace=1.7)
@@ -65,16 +78,16 @@ def generate_sugammadex_attestation(attestnr, patient_name, prescriber_first_nam
     canvas.drawImage('page1.jpg', x=0, y=0, width=page_width, height=page_height)
     canvas.drawString(0.8 * cm, 16.12 * cm, f"{prescriber_last_name}", charSpace=2.8)
     canvas.drawString(0.8 * cm, 15.45 * cm, f"{prescriber_first_name}", charSpace=2.8)
-    canvas.drawString(2.15 * cm, 14.7 * cm, "07903", charSpace=2.8)
-    canvas.drawString(4.35 * cm, 14.7 * cm, "54", charSpace=2.8)
-    canvas.drawString(5.40 * cm, 14.7 * cm, "010", charSpace=2.8)
+    canvas.drawString(2.15 * cm, 14.7 * cm, f"{riziv_case[0:5]}", charSpace=2.8)
+    canvas.drawString(4.35 * cm, 14.7 * cm, f"{riziv_case[5:]}", charSpace=2.8)
+    canvas.drawString(5.40 * cm, 14.7 * cm, f"{qualcode_case}", charSpace=2.8)
     canvas.drawString(0.8 * cm, 14.0 * cm, operation_date_day, charSpace=2.8)
     canvas.drawString(2.2 * cm, 14.0 * cm, operation_date_month, charSpace=2.8)
     canvas.drawString(3.3 * cm, 14.0 * cm, operation_date_year, charSpace=2.8)
     canvas.save()
 
 
-worksheet = pd.read_excel("source/december 2022.xlsx", engine='openpyxl', sheet_name="Sheet1")
+worksheet = pd.read_excel("source/februari 2022.xlsx", engine='openpyxl', sheet_name="Sheet1")
 worksheet=worksheet[worksheet['DESCRIPTION'] == "BRIDION 200 MG/2 ML FLAC"]
 
 for index, row in worksheet.iterrows():
@@ -83,16 +96,33 @@ for index, row in worksheet.iterrows():
     patient_INSZ = int(row['RIJKSREGNR'])
     aantal_flacons = int(row['AANTAL'])
     operation_date = str(row['DATUM'].date())
-    print(operation_date)
     prescriber = row['TOEGEWEZEN_VOORSCHRIJVER']
-    print(f"{patient_name} {patient_INSZ} {aantal_flacons} {operation_date} {prescriber}")
-
     prescriber_names = prescriber.split(' ')
     prescriber_first_name = prescriber_names[-1]
     prescriber_names.pop(-1)
     prescriber_last_name = " ".join(prescriber_names)
 
-    print(prescriber_first_name)
-    print(prescriber_last_name)
+    fields = "riziv_nr, qualification_code"
+    table = "anesthesie_uzgent"
+    conditions = f"first_name = '{prescriber_first_name}' AND last_name = '{prescriber_last_name}'"
 
-    generate_sugammadex_attestation(attestnr, patient_name=patient_name, prescriber_first_name=prescriber_first_name, prescriber_last_name=prescriber_last_name, datum=operation_date, dosage=4, flacons=aantal_flacons )
+    query = (f"SELECT {fields} "
+           f"FROM {table} "
+           f"WHERE {conditions};")
+
+    cursor.execute(query)
+    riziv = cursor.fetchone()
+
+    reason_query = """ 
+    SELECT reason FROM sugammadex_attestation  
+    ORDER BY random()
+    LIMIT 1;
+    """
+
+    cursor.execute(reason_query)
+    reason = cursor.fetchone()[0]
+
+#   try:
+    generate_sugammadex_attestation(attestnr, patient_name=patient_name, prescriber_first_name=prescriber_first_name, prescriber_last_name=prescriber_last_name, riziv=riziv, datum=operation_date, dosage=4, flacons=aantal_flacons, reason=reason)
+    #except TypeError:
+    #    print(f"Missing database entry for {prescriber_last_name} {prescriber_first_name}")
